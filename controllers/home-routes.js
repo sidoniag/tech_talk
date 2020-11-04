121c0c7ef366f49ba7ae2fe97667bda996553a75
idonia <sidoniag@gmail.com>
2020-11-03 19:55:04 -0600

    broke again

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -9,8 +9,7 @@ router.get('/', (req, res) => {
           'id',
           'post_url',
           'title',
-          'created_at',
-          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+          
         ],
         include: [
           {
--- models/User.js
+++ models/User.js
@@ -3,12 +3,16 @@ const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 // create our User model
-class User extends Model {
+class User extends Model {}
   // set up method to run on instance data (per user) to check password
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
+
+// // create our User model
+// class User extends Model {
+//   // set up method to run on instance data (per user) to check password
+//   checkPassword(loginPw) {
+//     return bcrypt.compareSync(loginPw, this.password);
+//   }
+// }
 
 // create fields/columns for User model
 User.init(
@@ -22,14 +26,6 @@ User.init(
     username: {
       type: DataTypes.STRING,
       allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
     },
     password: {
       type: DataTypes.STRING,
