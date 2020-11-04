af13a1767be8729e4ad474853da75515dbfcf51d
idonia <sidoniag@gmail.com>
2020-11-04 13:42:19 -0600

    move to develop

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,31 +1,11 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
     console.log(req.session)
     Post.findAll({
-        attributes: [
-          'id',
-          'post_url',
-          'title',
-          
-        ],
-        include: [
-          {
-            model: Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-            include: {
-              model: User,
-              attributes: ['username']
-            }
-          },
-          {
-            model: User,
-            attributes: ['username']
-          }
-        ]
-      })
+        include: [User],
+    })
         .then(dbPostData => {
           //console.log(dbPostData[0]);
           const posts = dbPostData.map(post => post.get({ plain: true }));
@@ -37,24 +17,46 @@ router.get('/', (req, res) => {
         });
 });
 
-router.get('/login', (req,res) => {
+router.get("/post/:id", (req,res) => {
+  Post.findByPk(req.params.id, {
+    include: [
+      User,
+      {
+        model: Comment,
+        include: [User],
+      },
+    ],
+  })
+  .then((dbPostData) => {
+    if (dbPostData) {
+      const post = dbPostData.get({ plain: true });
+
+      res.render("post", { post });
+    } else {
+      res.status(404).end();
+    }
+  })
+  .catch((err) => {
+    res.status(500).json(err);
+  })
+})
+
+router.get("/login", (req,res) => {
   if (req.session.loggedIn) {
-    res.redirect('/');
+    res.redirect("/login");
     return;
   }
 
   res.render('login');
 });
 
-router.post('/logout', (req, res) => {
+router.post('/signup', (req, res) => {
   if (req.session.loggedIn) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  }
-  else {
-    res.status(404).end();
-  }
-});
+    req.redirect('/');
+    return;
+    }
+
+    res.render("signup");
+  });
 
 module.exports = router;
