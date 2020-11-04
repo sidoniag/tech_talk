7d5b5e900731c7f18aa75b21944e4ea358f39266
idonia <sidoniag@gmail.com>
2020-11-04 17:09:03 -0600

    working on post

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
-
+// get all users
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: "all users" });
+    });
+});
 // POST /api/users
 router.post('/', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
--- models/index.js
+++ models/index.js
@@ -1,17 +1,17 @@
-// const Post = require('./Post');
+const Post = require('./Post');
 const User = require('./User');
 // const Comment = require('./Comment');
 
 // create associations
 // user can make many post
-// User.hasMany(Post, {
-//     foreignKey: 'user_id'
-//   });
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
 
 // a post can only belong to one user
-// Post.belongsTo(User, {
-//     foreignKey: 'user_id'
-//   });
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
 
 // Comment.belongsTo(User, {
 //     foreignKey: 'user_id'
@@ -29,5 +29,5 @@ const User = require('./User');
 //     foreignKey: 'post_id'
 //   });
 
-module.exports = { User };
+module.exports = { User, Post };
 // module.exports = { User, Post, Comment };
